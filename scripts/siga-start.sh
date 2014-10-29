8305c84ab2932d75f6ad4dbb89514824a50f1618
Markenson <markensonfranca@gmail.com>
2014-10-29 12:18:19 -0200

    Ajusta caracteres compativeis com linux

--- scripts/siga-start.sh
+++ scripts/siga-start.sh
@@ -3,4 +3,4 @@ docker run --name db.server -h db.server -d -p 49160:22 -p 49161:1521 -p 49162:8
 
 docker run --name app.server -h app.server -m=1g --link db.server:db.server -p 50000:8080 -t -i --rm siga/app.server
 
-docker run -d -t -–name web.server -h web.server -p 80:80 -–link app.server:app.server siga/web.server
+docker run -d -t --name web.server -h web.server -p 80:80 --link app.server:app.server siga/web.server
