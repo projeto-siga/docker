470259f0112c31229f7ecc35e7c905ace4a6dcef
Markenson <markensonfranca@gmail.com>
2014-10-29 12:03:10 -0200

    Define porta fixa para app.server e acrescenta web.server

--- scripts/siga-start.sh
+++ scripts/siga-start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 docker run --name db.server -h db.server -d -p 49160:22 -p 49161:1521 -p 49162:8080 siga/db.server
 
-docker  run --name app.server -h app.server -m=1g --link db.server:db.server -p 8080 -t -i --rm siga/app.server
+docker run --name app.server -h app.server -m=1g --link db.server:db.server -p 50000:8080 -t -i --rm siga/app.server
+
+docker run -d -t -–name web.server -h web.server -p 80:80 -–link app.server:app.server siga/web.server
