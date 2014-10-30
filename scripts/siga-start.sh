1fe4d836a4f791a72b3e9903ef8f77245ee37966
Markenson <markensonfranca@gmail.com>
2014-10-30 19:40:40 -0200

    Ajuste de inicializacao

--- scripts/siga-start.sh
+++ scripts/siga-start.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 echo "Iniciando db.server..."
-docker run --name db.server -h db.server -d -p 49160:22 -p 49161:1521 -p 49162:8080 siga/db.server
+docker run -d --name db.server -h db.server -p 49160:22 -p 49161:1521 -p 49162:8080 siga/db.server
+
+echo "Iniciando email.server..."
+docker run -d --name email.server -h email.server -p 49163:1080 -p 49164:25 previousnext/mailcatcher
 
 echo "Iniciando app.server..."
-docker run --name app.server -h app.server -m=1g --link db.server:db.server -p 50000:8080 -t -i --rm siga/app.server
+docker run -d --name app.server -h app.server -m=1g --link db.server:db.server --link email.server:email.server -p 50000:8080 siga/app.server
 
 echo "Iniciando web.server..."
-docker run -d -t --name web.server -h web.server -p 80:80 --link app.server:app.server siga/web.server
+docker run -d --name web.server -h web.server -p 80:80 --link app.server:app.server siga/web.server
 
 echo "OK!"
