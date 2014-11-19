4ac09d1e0721fc88da8704438c6535231be831a0
Markenson <markensonfranca@gmail.com>
2014-11-19 15:50:53 -0200

    Configura update automatico do flyway para versao experimental

--- scripts/siga-start.sh
+++ scripts/siga-start.sh
@@ -6,7 +6,7 @@ echo "Iniciando email.server..."
 docker run -d --name email.server -h email.server -p 49163:1080 -p 49164:25 previousnext/mailcatcher
 
 echo "Iniciando app.server..."
-docker run -d --name app.server -h app.server -m=1g --link db.server:db.server --link email.server:email.server -p 50000:8080 siga/app.server
+docker run -d --name app.server -h app.server -m=1g --link db.server:db.server --link email.server:email.server -e flyway_run=auto -p 50000:8080 siga/app.server
 
 echo "Iniciando web.server..."
 docker run -d --name web.server -h web.server -p 80:80 --link app.server:app.server siga/web.server /usr/sbin/httpd -DFOREGROUND 
