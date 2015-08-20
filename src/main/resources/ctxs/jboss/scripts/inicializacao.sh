b4e4e46bbc40cfd34c707dd96348036a36a0f47d
Markenson <markensonfranca@gmail.com>
2015-08-20 16:07:41 -0300

    Adapta arquivos para montagem da imagem do jboss 6

--- src/main/resources/ctxs/jboss/scripts/inicializacao.sh
+++ src/main/resources/ctxs/jboss/scripts/inicializacao.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-
+set -x
 #--- verifica conexao com db.server
 pass=$db_server_check_pass
 
@@ -24,94 +24,95 @@ done
 
 #--- inicializa banco de dados ---
 if [ "$init_db" == "on" ]
-	then
-	cmd_jdbctool=' | java -cp jdbctool.jar:lib/java-getopt-1.0.13.jar:lib/libreadline-java-0.8.0.jar:lib/ojdbc6.jar  com.quuxo.jdbctool.JdbcTool -u system -p oracle $db_server_check_url > /dev/null'
-	cmd_1='echo ALTER USER SYSTEM IDENTIFIED BY oracle'
-	cmd_2='echo CREATE USER corporativo IDENTIFIED BY corporativo default tablespace USERS'
-	cmd_3='echo CREATE USER siga IDENTIFIED BY siga default tablespace USERS'
-	cmd_4='echo CREATE USER sigawf IDENTIFIED BY sigawf default tablespace USERS'
-	cmd_5='echo CREATE USER sigasr IDENTIFIED BY sigasr default tablespace USERS'
-	cmd_6='echo CREATE USER sigagc IDENTIFIED BY sigagc default tablespace USERS'
-	cmd_7='echo CREATE USER sigapmp IDENTIFIED BY sigapmp default tablespace USERS'	
-	cmd_8='echo CREATE USER sigatp IDENTIFIED BY sigatp default tablespace USERS'		
-	cmd_9='echo GRANT CONNECT,RESOURCE TO CORPORATIVO'	
-	cmd_10='echo GRANT CONNECT,RESOURCE TO SIGA'
-	cmd_11='echo GRANT CONNECT,RESOURCE TO SIGAWF'
-	cmd_12='echo GRANT CONNECT,RESOURCE TO sigasr'
-	cmd_13='echo GRANT CONNECT,RESOURCE TO sigagc'
-	cmd_14='echo GRANT CONNECT,RESOURCE TO sigapmp'
-	cmd_15='echo GRANT CONNECT,RESOURCE TO sigatp'
-	echo $cmd_1
-	eval $cmd_1$cmd_jdbctool
-	echo $cmd_2
-	eval $cmd_2$cmd_jdbctool
-	echo $cmd_3
-	eval $cmd_3$cmd_jdbctool
-	echo $cmd_4
-	eval $cmd_4$cmd_jdbctool
-	echo $cmd_5
-	eval $cmd_5$cmd_jdbctool
-	echo $cmd_6
-	eval $cmd_6$cmd_jdbctool
-	echo $cmd_7
-	eval $cmd_7$cmd_jdbctool
-	echo $cmd_8
-	eval $cmd_8$cmd_jdbctool
-	echo $cmd_9
-	eval $cmd_9$cmd_jdbctool
-	echo $cmd_10
-	eval $cmd_10$cmd_jdbctool
-	echo $cmd_11
-	eval $cmd_11$cmd_jdbctool
-	echo $cmd_12
-	eval $cmd_12$cmd_jdbctool
-	echo $cmd_13
-	eval $cmd_13$cmd_jdbctool
-	echo $cmd_14
-	eval $cmd_14$cmd_jdbctool
-	echo $cmd_15
-	eval $cmd_15$cmd_jdbctool
+        then
+        cmd_jdbctool=' | java -cp jdbctool.jar:lib/java-getopt-1.0.13.jar:lib/libreadline-java-0.8.0.jar:lib/ojdbc6.jar  com.quuxo.jdbctool.JdbcTool -u system -p oracle $db_server_check_url > /dev/null'
+        cmd_1='echo ALTER USER SYSTEM IDENTIFIED BY oracle'
+        cmd_2='echo CREATE USER corporativo IDENTIFIED BY corporativo default tablespace USERS'
+        cmd_3='echo CREATE USER siga IDENTIFIED BY siga default tablespace USERS'
+        cmd_4='echo CREATE USER sigawf IDENTIFIED BY sigawf default tablespace USERS'
+        cmd_5='echo CREATE USER sigasr IDENTIFIED BY sigasr default tablespace USERS'
+        cmd_6='echo CREATE USER sigagc IDENTIFIED BY sigagc default tablespace USERS'
+        cmd_7='echo CREATE USER sigapmp IDENTIFIED BY sigapmp default tablespace USERS'
+        cmd_8='echo CREATE USER sigatp IDENTIFIED BY sigatp default tablespace USERS'
+        cmd_9='echo GRANT CONNECT,RESOURCE TO CORPORATIVO'
+        cmd_10='echo GRANT CONNECT,RESOURCE TO SIGA'
+        cmd_11='echo GRANT CONNECT,RESOURCE TO SIGAWF'
+        cmd_12='echo GRANT CONNECT,RESOURCE TO sigasr'
+        cmd_13='echo GRANT CONNECT,RESOURCE TO sigagc'
+        cmd_14='echo GRANT CONNECT,RESOURCE TO sigapmp'
+        cmd_15='echo GRANT CONNECT,RESOURCE TO sigatp'
+        echo $cmd_1
+        eval $cmd_1$cmd_jdbctool
+        echo $cmd_2
+        eval $cmd_2$cmd_jdbctool
+        echo $cmd_3
+        eval $cmd_3$cmd_jdbctool
+        echo $cmd_4
+        eval $cmd_4$cmd_jdbctool
+        echo $cmd_5
+        eval $cmd_5$cmd_jdbctool
+        echo $cmd_6
+        eval $cmd_6$cmd_jdbctool
+        echo $cmd_7
+        eval $cmd_7$cmd_jdbctool
+        echo $cmd_8
+        eval $cmd_8$cmd_jdbctool
+        echo $cmd_9
+        eval $cmd_9$cmd_jdbctool
+        echo $cmd_10
+        eval $cmd_10$cmd_jdbctool
+        echo $cmd_11
+        eval $cmd_11$cmd_jdbctool
+        echo $cmd_12
+        eval $cmd_12$cmd_jdbctool
+        echo $cmd_13
+        eval $cmd_13$cmd_jdbctool
+        echo $cmd_14
+        eval $cmd_14$cmd_jdbctool
+        echo $cmd_15
+        eval $cmd_15$cmd_jdbctool
 fi
 
 
 #--- atualiza banco de dados ---
 if [ "$flyway_run" == "on" -o "$flyway_run" == "auto" ]
-	then
-		cd /
-		rm -rf /siga/flyway-3.0/siga-updates
-		mkdir /siga/flyway-3.0/siga-updates
+        then
+                cd /
+                rm -rf /siga/flyway-3.0/siga-updates
+                mkdir /siga/flyway-3.0/siga-updates
 
-		unzip -j /siga/jboss-eap-5.2/jboss-as/server/sigadoc/deploy/siga.war WEB-INF/lib/siga-cp-0.0.1-SNAPSHOT.jar -d /siga/flyway-3.0/siga-updates
-		unzip  /siga/flyway-3.0/siga-updates/siga-cp-0.0.1-SNAPSHOT.jar db/migration/* -d /siga/flyway-3.0/sql/corporativo/
+                unzip -j /opt/jboss/sigadoc/deployments/siga.war WEB-INF/lib/siga-cp-1.2-SNAPSHOT.jar  -d /siga/flyway-3.0/siga-updates
+                unzip  /siga/flyway-3.0/siga-updates/siga-cp-1.2-SNAPSHOT.jar db/migration/* -d /siga/flyway-3.0/sql/corporativo/
 
-		unzip -j /siga/jboss-eap-5.2/jboss-as/server/sigadoc/deploy/sigaex.war WEB-INF/lib/siga-ex-0.0.1-SNAPSHOT.jar -d /siga/flyway-3.0/siga-updates
-		unzip  /siga/flyway-3.0/siga-updates/siga-ex-0.0.1-SNAPSHOT.jar db/migration/* -d /siga/flyway-3.0/sql/siga/
+                unzip -j /opt/jboss/sigadoc/deployments/sigaex.war WEB-INF/lib/siga-ex-1.2-SNAPSHOT.jar -d /siga/flyway-3.0/siga-updates
+                unzip  /siga/flyway-3.0/siga-updates/siga-ex-1.2-SNAPSHOT.jar  db/migration/* -d /siga/flyway-3.0/sql/siga/
 
-		unzip -j /siga/jboss-eap-5.2/jboss-as/server/sigadoc/deploy/sigawf.war WEB-INF/lib/siga-wf-0.0.1-SNAPSHOT.jar -d /siga/flyway-3.0/siga-updates
-		unzip  /siga/flyway-3.0/siga-updates/siga-wf-0.0.1-SNAPSHOT.jar db/migration/* -d /siga/flyway-3.0/sql/sigawf/
+                unzip -j /opt/jboss/sigadoc/deployments/sigawf.war WEB-INF/lib/siga-wf-1.2-SNAPSHOT.jar -d /siga/flyway-3.0/siga-updates
+                unzip  /siga/flyway-3.0/siga-updates/siga-wf-1.2-SNAPSHOT.jar db/migration/* -d /siga/flyway-3.0/sql/sigawf/
 
-		#--- aplicacoes play ---
-		unzip -j /siga/jboss-eap-5.2/jboss-as/server/sigadoc/deploy/sigasr.war WEB-INF/application/app/db/* -d /siga/flyway-3.0/siga-updates/sigasr
-		mkdir -p /siga/flyway-3.0/sql/sigasr/
-		cp /siga/flyway-3.0/siga-updates/sigasr/*  /siga/flyway-3.0/sql/sigasr/
-		
-		unzip -j /siga/jboss-eap-5.2/jboss-as/server/sigadoc/deploy/sigagc.war WEB-INF/application/app/db/* -d /siga/flyway-3.0/siga-updates/sigagc
-		mkdir -p /siga/flyway-3.0/sql/sigagc/
-		cp /siga/flyway-3.0/siga-updates/sigagc/*  /siga/flyway-3.0/sql/sigagc/
+                #--- aplicacoes play ---
+                unzip -j /opt/jboss/sigadoc/deployments/sigasr.war WEB-INF/classes/db/* -d /siga/flyway-3.0/siga-updates/sigasr
+                mkdir -p /siga/flyway-3.0/sql/sigasr/
+                cp /siga/flyway-3.0/siga-updates/sigasr/*  /siga/flyway-3.0/sql/sigasr/
+
+                unzip -j /opt/jboss/sigadoc/deployments/sigagc.war WEB-INF/classes/db/* -d /siga/flyway-3.0/siga-updates/sigagc
+                mkdir -p /siga/flyway-3.0/sql/sigagc/
+                cp /siga/flyway-3.0/siga-updates/sigagc/*  /siga/flyway-3.0/sql/sigagc/
 
 
-		/siga/flyway-3.0/flyway -configFile=conf/flyway.corporativo.properties migrate
-		/siga/flyway-3.0/flyway -configFile=conf/flyway.siga.properties migrate
-		/siga/flyway-3.0/flyway -configFile=conf/flyway.sigawf.properties migrate
-		
-		/siga/flyway-3.0/flyway -configFile=conf/flyway.sigasr.properties migrate
-		/siga/flyway-3.0/flyway -configFile=conf/flyway.sigagc.properties migrate
+                /siga/flyway-3.0/flyway -configFile=conf/flyway.corporativo.properties migrate
+                /siga/flyway-3.0/flyway -configFile=conf/flyway.siga.properties migrate
+                /siga/flyway-3.0/flyway -configFile=conf/flyway.sigawf.properties migrate
 
-		rm -rf /siga/flyway-3.0/siga-updates/*
-	
+                /siga/flyway-3.0/flyway -configFile=conf/flyway.sigasr.properties migrate
+                /siga/flyway-3.0/flyway -configFile=conf/flyway.sigagc.properties migrate
+
+                rm -rf /siga/flyway-3.0/siga-updates/*
+
 fi
 
 if [ "$flyway_run" == "off" -o "$flyway_run" == "auto" ]
-	then
-		/siga/jboss-eap-5.2/jboss-as/bin/run.sh -b 0.0.0.0 -c sigadoc
+        then
+              /opt/jboss/bin/init.d/sigadoc.sh start
 fi
+
