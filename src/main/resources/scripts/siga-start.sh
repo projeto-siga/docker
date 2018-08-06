66557e3857d1aeeb0c6d030179b6a37cf4756342
Renato Crivano <renato.crivano@gmail.com>
2018-08-06 12:36:22 -0300

    Incluindo o servidor do GraphViz

--- src/main/resources/scripts/siga-start.sh
+++ src/main/resources/scripts/siga-start.sh
@@ -5,11 +5,14 @@ docker run -d --name db.server -h db.server -p 49160:22 -p 49161:1521 -p 49162:8
 echo "Iniciando email.server..."
 docker run -d --name email.server -h email.server -p 49163:1080 -p 49164:25 schickling/mailcatcher
 
+echo "Iniciando viz.server..."
+docker run -d --name viz.server -h viz.server -p 49165:8080 omerio/graphviz-server 8080
+
 echo "Iniciando bluc.server..."
 docker run -d --name bluc.server -h bluc.server -p 50010:8080 siga/bluc.server
 
 echo "Iniciando app.server..."
-docker run -d --name app.server -h app.server -m=2g --link db.server:db.server --link email.server:email.server --link bluc.server:bluc.server -e init_db=on -e flyway_run=auto -p 50000:8080 -p 9990:9990 siga/app.server
+docker run -d --name app.server -h app.server -m=2g --link db.server:db.server --link email.server:email.server --link bluc.server:bluc.server --link viz.server:viz.server -e init_db=on -e flyway_run=auto -p 50000:8080 -p 9990:9990 siga/app.server
 
 echo "Iniciando web.server..."
 docker run -d --name web.server -h web.server -p 80:80 --link app.server:app.server siga/web.server  
@@ -31,6 +34,7 @@ echo "docker logs -f db.server"
 echo "docker logs -f app.server"
 echo "docker logs -f web.server"
 echo "docker logs -f email.server"
+echo "docker logs -f viz.server"
 echo ""
 echo ""
 echo ""
@@ -43,6 +47,8 @@ echo "JBOSS (siga bypass apache): http://192.168.59.103:50000/siga"
 echo "JBOSS (web admin): http://192.168.59.103:8080"
 echo ""
 echo "Apache: http://192.168.59.103"
+echo ""
+echo "GraphViz server: curl -X POST -d 'digraph G { a -> b -> c; a-> c; b -> d; c-> d; }' http://192.168.59.103:49165/svg"
 echo ""
 echo "STMP server (fake): 192.168.59.103 porta 49164"
 echo "STMP server (fake): (visualizar emails): http://192.168.59.103:49163"
